657357774ccf9aea15c16abb9edbe705c117f1ad
Zhenyu Shi <zhenyu.shi@compass.com>
2022-10-25 14:43:22 +0800

    docs: goroutine

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	for i := 0; i < 10; i++ {
-		func(i int) {
+	for i := 0; i < 1000; i++ {
+		go func(i int) {
 			for {
 				fmt.Printf("Hello goroutine %d\n", i)
 			}
 		}(i)
 	}
+	time.Sleep(time.Minute)
 }
