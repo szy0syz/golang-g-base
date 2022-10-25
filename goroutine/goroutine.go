a73fa439c23f5170b781ebb50f8232871678b8d5
Zhenyu Shi <zhenyu.shi@compass.com>
2022-10-25 15:17:06 +0800

    docs: 非抢占式 & 调度器

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
-	for i := 0; i < 1000; i++ {
+	var a [10]int
+	for i := 0; i < 10; i++ {
 		go func(i int) {
 			for {
-				fmt.Printf("Hello goroutine %d\n", i)
+				a[i]++
+				runtime.Gosched()
 			}
 		}(i)
 	}
-	time.Sleep(time.Minute)
+	time.Sleep(time.Millisecond * 1000)
+	fmt.Println(a)
 }
