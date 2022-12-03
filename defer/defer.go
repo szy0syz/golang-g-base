ec0fcbc32fd140c38ba0a3018df231d80c51162b
Zhenyu Shi <zhenyu.shi@compass.com>
2022-12-03 15:12:55 +0800

    chore: defer demo

---
+++ defer/defer.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func writeFile(filename string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	for i := 0; i < 20; i++ {
+		fmt.Fprintln(writer, i)
+	}
+}
+
+func main() {
+	writeFile("1.txt")
+}
