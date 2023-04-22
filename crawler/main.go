96ee236b06eb9231c8a1788d1cb96b416738672e
Zhenyu Shi <zhenyu.shi@compass.com>
2023-04-22 22:50:30 +0800

    Update crawler1

--- crawler/main.go
+++ crawler/main.go
@@ -1,44 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/transform"
-	"io"
-	"io/ioutil"
-	"net/http"
+	"g-base/crawler/fetcher"
+	"regexp"
 )
 
 func main() {
-	resp, err := http.Get("https://www.zhenai.com/zhenghun")
+	all, err := fetcher.Fetch("http://www.zhenai.com/zhenghun")
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error: status code", resp.StatusCode)
-		return
-	}
-
-	e := determineEncoding(resp.Body)
-	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
-	all, err := ioutil.ReadAll(utf8Reader)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%s\n", all)
-
+	printCityList(all)
 }
 
-func determineEncoding(r io.Reader) encoding.Encoding {
-	bytes, err := bufio.NewReader(r).Peek(1024)
-	if err != nil {
-		panic(err)
+func printCityList(contexts []byte) {
+	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+	matches := re.FindAllSubmatch(contexts, -1)
+	for _, m := range matches {
+		fmt.Printf("City: %s, URL:%s\n", m[2], m[1])
 	}
-
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
 }
