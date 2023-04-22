256f425fa9361fc70c70867232705130ae22d656
Zhenyu Shi <zhenyu.shi@compass.com>
2023-04-22 23:11:19 +0800

    Update main.go

--- crawler/main.go
+++ crawler/main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"g-base/crawler/fetcher"
-	"regexp"
+	"g-base/crawler/engine"
+	"g-base/crawler/zhenai/parser"
 )
 
 func main() {
-	all, err := fetcher.Fetch("http://www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-
-	printCityList(all)
-}
-
-func printCityList(contexts []byte) {
-	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
-	matches := re.FindAllSubmatch(contexts, -1)
-	for _, m := range matches {
-		fmt.Printf("City: %s, URL:%s\n", m[2], m[1])
-	}
+	engine.Run(engine.Request{
+		Url:        "http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
 }
